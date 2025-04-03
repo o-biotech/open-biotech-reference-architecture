d8d1cfcc75732f7aa037de96f508d717beb8b704
mcgear <michael.gearhardt@gmail.com>
2025-04-03 12:30:24 -0600

    missing values

--- src/state/OpenBiotechWebAPIState.ts
+++ src/state/OpenBiotechWebAPIState.ts
@@ -1,5 +1,9 @@
 import { OpenBiotechAPIJWTPayload } from './OpenBiotechAPIJWTPayload.ts';
 
 export type OpenBiotechWebAPIState = {
+  CloudLookup?: string;
+
   EaCJWT?: string;
+
+  ResourceGroupLookup?: string;
 } & Omit<OpenBiotechAPIJWTPayload, 'JWT'>;
